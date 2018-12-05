47c804ffb9d24f9abf59e0bdae68704dd57f269a
linsen <linsen80586@163.com>
2018-12-05 12:15:02 +0800

    add ReactNativeNavigation

--- App.js
+++ App.js
@@ -8,7 +8,8 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import { Button } from 'react-native';
+import {Button} from 'react-native';
+import {Navigation} from 'react-native-navigation';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
